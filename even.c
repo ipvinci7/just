4b31dc2a31bba9617fc9461da654301804423e5d
ipvinci7 <ipvinci7@gmail.com>
2026-01-22 21:39:54 +0100

    even function

---
+++ even.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int even(int num);
+int main()
+{
+
+}
+
+int even(int num)
+{
+    if(num%2==0)
+    {
+        return  1;
+    }
+    else
+    {
+        return 0;
+    }
+
+}
