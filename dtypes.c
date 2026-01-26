8cb5a64f5b69ccf8f9587b31cebedfc05891ed4a
ipvinci7 <ipvinci7@gmail.com>
2026-01-26 17:19:47 +0100

    added spice to stringlength

--- dtypes.c
+++ dtypes.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
-
+  enum boolean {NO,YES};
+enum escapes {BELL = '\a',BACKSPACE = '\b',TAB = '\t',NEWLINE='\n',VTAB='\v',RETURN = '\r'};
+int strlength(const char word[]);
 int main()
 {
+  
     int  ox1f;
     printf("%d\n",ox1f);
+    char name[] = "vinci";
+    int length;
+    length = strlength("vinci");
+    printf("The length of the word is %d\n",length);
 }
 
 
-int strlength(char word[])
+int strlength(const char word[])
 {
+    
     int i = 0;
-    while(word[0]!='\0')
+    while(word[i]!='\0')
     {
         i++;
     }
     return i;
 }
+//enumeration constant is a list of constant integer values as in
+
+
