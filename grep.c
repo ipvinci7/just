3e095e43be9abab0680c50a84a066fb41a6ea359
ipvinci7 <ipvinci7@gmail.com>
2026-01-31 23:03:42 +0100

    grep

---
+++ grep.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+
+}
+
+
+int grepped(char word[],char find[])
+{
+    //the idea is to circle the the word andn
+    for(int i=0;i<sizeof(word);i++)
+    {
+         for(int j=0;j<size)
+    } 
+}
