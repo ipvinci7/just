e91a4cdcca9f61d5040528a3cef65e825cf2c74a
ipvinci7 <ipvinci7@gmail.com>
2026-02-02 16:04:15 +0100

    bitcount

--- grep.c
+++ grep.c
@@ -32,3 +32,28 @@ int grepped(char word[],char find[])
    else 
    return -1;
 }
+
+int grepped(char word[], char find[])
+{
+    int i, j, k;
+    int wordlen = strlen(word);
+    int findlen = strlen(find);
+    
+    // If find is longer than word, it can't be a substring
+    if (findlen > wordlen)
+        return -1;
+    
+    // Check for substring starting at each position in word
+    for (i = 0; i <= wordlen - findlen; i++) {
+        // Check if find matches starting at position i
+        for (j = 0, k = i; j < findlen; j++, k++) {
+            if (word[k] != find[j])
+                break;
+        }
+        // If we matched all characters of find
+        if (j == findlen)
+            return i;  // Return starting position
+    }
+    
+    return -1;  // Not found
+}
