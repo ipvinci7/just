581bfdf47c85fd00045656306712fe9115337e8e
ipvinci7 <ipvinci7@gmail.com>
2026-02-05 19:06:29 +0100

    vv

--- countdigit.c
+++ countdigit.c
@@ -12,3 +12,29 @@ int main()
   ++ndigit[c-'0'];
   
 }
+
+
+int getline(char word[100],int lim)
+{
+   int c,i;
+   for(i=0;i<lim-1 && (c=getchar())!=EOF && c!='\n';++i)
+    word[i] = c;
+    if(c=='\n')
+    {
+       word[i] = c;
+       ++i;
+    }
+    word[i] = '\0';
+    return i;
+}
+
+void copy(char from[100],char to[100])
+{
+  int i = 0;
+  while(from[i]!='\0')
+  {
+     to[i] = from[i];
+     i++;
+  }
+  from[i] = '\0';
+}
