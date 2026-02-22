47db15d7c7314bf7dccdef84c9e204f4487cb118
ipvinci7 <ipvinci7@gmail.com>
2026-02-22 17:35:21 +0100

    testing add

--- personallibrary/main.c
+++ personallibrary/main.c
@@ -4,26 +4,34 @@ and save/load the library to a file so data persists between runs.
 Key Concepts: structs to model a book, arrays of structs, functions, and most importantly, file input/output (fopen, fprintf, fscanf, fclose). File I/O is crucial for understanding how programs store data and state.*/
 #include <stdio.h>
 #include <string.h>
-void addbook(struct book b ,char title[20],char author[20],char edition[20],int n)
-{
-    strcpy(b.title,b.title);
-    //return 1;
-}
+// void addbook(struct book b ,char title[20],char author[20],char edition[20],int n)
+// {
+//     strcpy(b.title,b.title);
+//     //return 1;
+// }
+
 struct book{
     char title[50];
     char author[20];
     char edition[20];
     int number;
 };
-
+void showbook(struct book b)
+{
+    printf("Title: %s",b.title);
+    printf("\n author: %s",b.author);
+    printf("\n Edition %s",b.edition);
+    printf("\n number %d",b.number);
+}
 
 int main()
 {
      struct book book1;
-    
-     addbook(book1,"Half of a yellow sun","Chimamada Adiche","version III",9);
+     strcpy(book1.title,"Halfofyellowsun");
+    showbook(book1);
+    //  addbook(book1,"Half of a yellow sun","Chimamada Adiche","version III",9);
 
-     printf("%s",book1.title);
+    //  printf("%s",book1.title);
 }
 
 
---
+++ personallibrary/structpractice.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct car 
+{
+     char name[30];
+     int price;
+};
+void print_car_info(struct car c)
+{
+     printf("Name : %s",c.name);
+     printf("\n Price : %d\n",c.price);
+}
+int main()
+{
+   struct car c = {"Tata",1021};
+   print_car_info(c);
+   return 0;
+}
+
+
+//passing struct as an argument the whole thing is going to be copied 
+//1.pass the whole thing 
+//2. By passing the entire structure to the function
