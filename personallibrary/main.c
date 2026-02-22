8aa522a46beff507a1691148159003d24dd16305
ipvinci7 <ipvinci7@gmail.com>
2026-02-22 15:46:50 +0100

    testing book struct

--- personallibrary/main.c
+++ personallibrary/main.c
@@ -2,10 +2,18 @@
 Users should be able to add a book (title, author), list all books, search for a book by title, 
 and save/load the library to a file so data persists between runs.
 Key Concepts: structs to model a book, arrays of structs, functions, and most importantly, file input/output (fopen, fprintf, fscanf, fclose). File I/O is crucial for understanding how programs store data and state.*/
-
+#include <stdio.h>
 struct book{
     char title[20];
     char author[20];
     char edition[20];
     int number;
 };
+
+
+int main()
+{
+     struct book book1;
+     book1.number =1;
+     printf("%d\n",book1.number);
+}
