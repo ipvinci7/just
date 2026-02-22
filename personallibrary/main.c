950ed712b6f69723ba1bfc5130ffe2d92040e134
ipvinci7 <ipvinci7@gmail.com>
2026-02-22 15:42:56 +0100

    libraryproject

---
+++ personallibrary/main.c
@@ -0,0 +1,11 @@
+/*Project: Create a text-based program to manage a collection of books. 
+Users should be able to add a book (title, author), list all books, search for a book by title, 
+and save/load the library to a file so data persists between runs.
+Key Concepts: structs to model a book, arrays of structs, functions, and most importantly, file input/output (fopen, fprintf, fscanf, fclose). File I/O is crucial for understanding how programs store data and state.*/
+
+struct book{
+    char title[20];
+    char author[20];
+    char edition[20];
+    int number;
+};
