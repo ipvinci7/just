c7d9d1dedc3c1a243665d6169fa84be31f11841d
ipvinci7 <ipvinci7@gmail.com>
2026-02-25 14:54:58 +0100

    student struct

--- personallibrary/structpractice.c
+++ personallibrary/structpractice.c
@@ -10,8 +10,15 @@ void print_car_info(struct car c)
      printf("Name : %s",c.name);
      printf("\n Price : %d\n",c.price);
 }
+struct student
+{
+      char name[50];
+      int age;
+};
+void display(struct student s);
 int main()
 {
+   struct student s1;
    struct car c = {"Tata",1021};
    print_car_info(c);
    return 0;
