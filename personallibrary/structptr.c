70c5544531482da16f957004ff2594cecdef359e
ipvinci7 <ipvinci7@gmail.com>
2026-02-28 22:48:16 +0100

    s1 struct

--- personallibrary/structptr.c
+++ personallibrary/structptr.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-
+#include <string.h>
 struct Car
 {
     char brand[30];
     int year;
+};
+struct Student
+{
+    char name[70];
+    char class;
+    int age;
 };
 int main()
 {
@@ -14,4 +20,11 @@ int main()
     //ACCESSING THE MEMBERS OF VARIABLE 
     printf("Brand: %s\n",ptr->brand);
     printf("Year:%d\n",ptr->year);
+
+    struct Student s1;
+    s1.age = 28;
+    strcpy(s1.name,"Vinci");
+    struct Student *s1ptr = &s1;
+    printf("The students age %d\n",s1ptr->age);
+    printf("The students name is %s\n",s1ptr->name);
 }
