266f5701f6815201cc050e4d72de52c7e51d0b2c
ipvinci7 <ipvinci7@gmail.com>
2026-03-08 15:52:47 +0100

    working on util

--- personallibrary/util.c
+++ personallibrary/util.c
@@ -1,12 +1,32 @@
 #include<stdio.h>
-
+#include <string.h>
+int find(char word[],char example[]);
 int main()
 {
-
+   char name[10] ="vinci";
+   char name1[10] = "vincii";
+   find(name,name1);
+   printf("%s\n",name);
 }
 
 
-int find(char word[100],char example[50])
+int find(char word[],char example[])
 {
-    
+    int i=0;
+    int j = 0;
+    //int len = strlen(word);
+    while(word[i]!='\0')
+    {
+         if(word[i]=example[j])
+         i++;
+         j++;
+    }
+    if(i==j)
+    {
+        printf("The words are same");
+    }
+    else
+    {
+        printf("The words are not the same");
+    }
 }
