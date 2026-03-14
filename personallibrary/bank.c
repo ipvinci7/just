2c7b3aa02f0c6e5f3bff075fb110280244e52fbc
ipvinci7 <ipvinci7@gmail.com>
2026-03-14 21:42:39 +0100

    add squeeze

--- personallibrary/bank.c
+++ personallibrary/bank.c
@@ -100,3 +100,11 @@ int findbyid()
     int s;
     ptr = fopen("bank.txt","r");
 }
+void squeeze(char s[],int c)
+{
+    int i,j;
+    for(i=j=0;s[i]!='\0';i++)
+    if(s[i]!=c)
+    s[j++] = s[i];
+    s[j] = '\0';
+}
