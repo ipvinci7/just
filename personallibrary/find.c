8e59c31cb04bd1f4782e39b270c094778b8f5ef1
ipvinci7 <ipvinci7@gmail.com>
2026-03-19 22:36:37 +0100

    extracting and finding id

---
+++ personallibrary/find.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct stud
+{
+    char id[4];
+    char name[20];
+    int age;
+};
+
+int main()
+{
+
+}
+
+//iterate the char id and return true if the id fits
+
+int find(char word[],)
